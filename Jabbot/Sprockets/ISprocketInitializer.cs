bc8544731da5335c007e2d15a681dee88d5efa3c
sethwebster <sethwebster@gmail.com>
2011-12-21 06:58:30 -0700

    Sorted usings.

--- Jabbot/Sprockets/ISprocketInitializer.cs
+++ Jabbot/Sprockets/ISprocketInitializer.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.ComponentModel.Composition;
+﻿using System.Linq;
+using System.Text;
 
 namespace Jabbot.Sprockets
 {
