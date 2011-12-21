0c16e65cb7c5e561369d3adcb9a1881347b05637
sethwebster <sethwebster@gmail.com>
2011-12-20 22:26:54 -0700

    More on Initalization strategy

--- Jabbot/Bot.cs
+++ Jabbot/Bot.cs
@@ -354,7 +354,7 @@ namespace Jabbot
         private void AddSprockets(CompositionContainer container)
         {
             // Add all the sprockets to the sprocket list
-            foreach (var sprocket in container.GetExportedValues<ISprocket>())
+            foreach (var sprocket in vals)
             {
                 Trace.WriteLine(String.Format("Adding sprocket {0}...", sprocket.GetType().Name));
                 AddSprocket(sprocket);
