18097a8078b2b7c8f00f2a3cdac9cb4d8c440d25
sethwebster <sethwebster@gmail.com>
2011-12-21 06:56:06 -0700

    Re-adding Initializer

---
+++ Jabbot/Sprockets/ISprocketInitializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+
+namespace Jabbot.Sprockets
+{
+    [InheritedExport]
+    public interface ISprocketInitializer
+    {
+        void Initialize(Bot bot);
+    }
+}
