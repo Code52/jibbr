02abec795c02d27f1bde4fbb5945c17512df0d8d
Brendan Forster <me@brendanforster.com>
2012-01-11 10:55:45 +1100

    find all subfolders under Extensions and copy assemblies

--- Jabbot.ConsoleBotHost/get-packages.cmd
+++ Jabbot.ConsoleBotHost/get-packages.cmd
@@ -1,9 +1,8 @@
-@echo off
-
 set solution_dir=%1
 set output_dir=%solution_dir%\Jabbot.ConsoleBotHost\bin\Debug\Sprockets\
 
 del %solution_dir%\Jabbot.ConsoleBotHost\bin\Debug\Sprockets\*.dll
-xcopy %solution_dir%\Extensions\SampleAnnouncement\bin\Debug\*.dll %output_dir% /C /Y
-xcopy %solution_dir%\Extensions\SampleSprocket\bin\Debug\*.dll %output_dir% /C /Y
-xcopy %solution_dir%\Extensions\HelpSprocket\bin\Debug\*.dll %output_dir% /C /Y
+
+for /D %%I in ("%solution_dir%\Extensions\*") do (
+xcopy %%I\bin\Debug\*.dll %output_dir% /C /Y
+)
