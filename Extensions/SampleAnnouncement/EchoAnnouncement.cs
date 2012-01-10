de717be23d656ad315e2b70a8e45c2a1938ec169
Brendan Forster <me@brendanforster.com>
2012-01-10 21:27:18 +1100

    implementing the interface for IAnnounce

---
+++ Extensions/SampleAnnouncement/EchoAnnouncement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using Jabbot;
+using Jabbot.CommandSprockets;
+
+namespace SampleAnnouncement
+{
+    public class EchoAnnouncement : IAnnounce
+    {
+        public TimeSpan Interval
+        {
+            get { return TimeSpan.FromHours(1); }
+        }
+
+        public void Execute(Bot bot)
+        {
+            var offset = TimeZoneInfo.FindSystemTimeZoneById("Australian Eastern Standard Time");
+            var now = DateTime.UtcNow.Add(offset.BaseUtcOffset);
+
+            var message = string.Format("The time in AEDST (Sydney time) is now {0}", now.ToString("hh:mm:ss, dd MMMM yy"));
+
+            foreach (var room in bot.Rooms)
+            {
+                bot.Say(message, room);
+            }
+        }
+    }
+}
