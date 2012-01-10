080df9c0aee1d54f806a2fa63b208debcdf9275f
Stefan D. Poulsen <cyberzed@sleddog.dk>
2012-01-11 00:04:09 +0100

    I PITY YOU FOOLS!

--- Jabbot.ConsoleBotHost/get-packages.cmd
+++ Jabbot.ConsoleBotHost/get-packages.cmd
@@ -8,3 +8,5 @@ copy "%solution%\Extensions\SampleSprocket\bin\Debug\*.dll" "%solution%\Jabbot.C
 copy "%solution%\Extensions\SampleSprocket\bin\Debug\*.pdb" "%solution%\Jabbot.ConsoleBotHost\bin\Debug\Sprockets\"
 copy "%solution%\Extensions\TwitterAnnouncer\bin\Debug\*.dll" "%solution%\Jabbot.ConsoleBotHost\bin\Debug\Sprockets\"
 copy "%solution%\Extensions\TwitterAnnouncer\bin\Debug\*.pdb" "%solution%\Jabbot.ConsoleBotHost\bin\Debug\Sprockets\"
+copy "%solution%\Extensions\IPityTheFoolSprocket\bin\Debug\*.dll" "%solution%\Jabbot.ConsoleBotHost\bin\Debug\Sprockets\"
+copy "%solution%\Extensions\IPityTheFoolSprocket\bin\Debug\*.pdb" "%solution%\Jabbot.ConsoleBotHost\bin\Debug\Sprockets\"
