d6c87f9259980fb524ae9ef1ced2036f56dcd82a
Brendan Forster <me@brendanforster.com>
2012-01-11 15:20:06 +1100

    added APIs to call

--- Extensions/GithubAnnouncements/GithubAnnouncements.cs
+++ Extensions/GithubAnnouncements/GithubAnnouncements.cs
@@ -9,25 +9,53 @@ namespace GithubAnnouncements
 {
     public class GithubAnnouncements : IAnnounce
     {
+        const string ProjectCommitsFeed = "/commits";
+        const string ProjectPullRequestsFeed = "/pulls";
+        const string ProjectWatchersFeed = "/watchers";
+        const string ProjectForksFeed = "/forks";
+        const string ProjectIssuesFeed = "/issues";
+
+        private readonly ILocalStorage _storage;
+        private readonly string _apiUrl;
+        readonly WebClient _client = new WebClient();
+        
+        public GithubAnnouncements(ILocalStorage storage, string apiUrl)
+        {
+            _storage = storage;
+            _apiUrl = apiUrl;
+        }
+
         public TimeSpan Interval
         {
-            get { return TimeSpan.FromMinutes(5); }
+            get { return TimeSpan.Zero; }
         }
 
         public void Execute(Bot bot)
         {
-            // TODO: query these services
-            // fetch commit feed from https://github.com/Code52/jibbr/commits/master.atom
-            // outstanding pull requests: https://api.github.com/repos/Code52/jibbr/pulls
-            // current issues: https://api.github.com/repos/Code52/jibbr/issues
-            // current forks: https://api.github.com/repos/Code52/jibbr/forks
-            // current watchers: https://api.github.com/repos/Code52/jibbr/watchers
+            var commits = GetResponse<IEnumerable<dynamic>>(GetFullUrl(ProjectCommitsFeed));
+            // TODO: message if new commits found
 
-            var client = new WebClient();
-            var commitsResponse = client.DownloadString("https://api.github.com/repos/Code52/jibbr/pulls");
+            return;
+            var pullRequests = GetResponse<IEnumerable<dynamic>>(GetFullUrl(ProjectPullRequestsFeed));
+            // TODO: message if new pull requests raised
+            // TODO: message if pull requests merged
+            var forks = GetResponse<IEnumerable<dynamic>>(GetFullUrl(ProjectForksFeed));
+            // TODO: inspect forks for new commits
+            var issues = GetResponse<IEnumerable<dynamic>>(GetFullUrl(ProjectIssuesFeed));
+            // TODO: message if new issues raised
+            var watchers = GetResponse<IEnumerable<dynamic>>(GetFullUrl(ProjectWatchersFeed));
+            // TODO: check for new watchers (or people no longer watching) and notify
+        }
 
-            var commits = JsonConvert.DeserializeObject<IEnumerable<dynamic>>(commitsResponse);
+        private string GetFullUrl(string feedLink)
+        {
+            return string.Format("{0}{1}", _apiUrl, feedLink);
+        }
 
+        private T GetResponse<T>(string url)
+        {
+            var response = _client.DownloadString(url);
+            return JsonConvert.DeserializeObject<T>(response);
         }
     }
 }
---
+++ Extensions/GithubAnnouncements/ILocalStorage.cs
@@ -0,0 +1,6 @@
+namespace GithubAnnouncements
+{
+    public interface ILocalStorage
+    {
+    }
+}
