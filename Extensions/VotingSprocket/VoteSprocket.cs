37c6d100afaa502fd615fdb27c11df541d43652e
David Roberts <david.roberts63@gmail.com>
2012-01-13 23:29:01 -0600

    Red light public poll messages

---
+++ Extensions/VotingSprocket/VoteSprocket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Jabbot.Sprockets.Core;
+using Jabbot.Models;
+using Jabbot;
+
+namespace VotingSprocket
+{
+    public class VoteSprocket : IMessageHandler
+    {
+        public bool Handle(ChatMessage message, Bot bot)
+        {
+            return true;
+        }
+    }
+}
---
+++ Extensions/VotingSprocketTests/VoteSprocketTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+using Jabbot;
+using VotingSprocket;
+using Jabbot.Models;
+
+namespace VotingSprocketTests
+{
+    [TestClass]
+    public class VoteSprocketTests
+    {
+        [TestMethod]
+        public void HandleShouldNotHandleNonPrivatePollMessage()
+        {
+            var bot = new Bot("", "myself", "");
+            var message = new ChatMessage("poll", "someoneelse", "theroom");
+            var sprocket = new VoteSprocket();
+            
+            bool wasHandled = sprocket.Handle(message, bot);
+
+            Assert.IsFalse(wasHandled, "Should not have handeled a public poll message.");
+        }
+    }
+}
