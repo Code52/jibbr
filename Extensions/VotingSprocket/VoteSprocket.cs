a4e2951aff69e1c76487fc705f1953069f9185ba
David Roberts <david.roberts63@gmail.com>
2012-01-14 01:58:58 -0600

    Green light on general functionality still needs to product instructional use

--- Extensions/VotingSprocket/VoteSprocket.cs
+++ Extensions/VotingSprocket/VoteSprocket.cs
@@ -6,24 +6,84 @@ using Jabbot.Sprockets.Core;
 using Jabbot.Models;
 using Jabbot;
 using System.Text.RegularExpressions;
+using System.Timers;
 
 namespace VotingSprocket
 {
-    public class VoteSprocket : IMessageHandler
+    public class VoteSprocket : ISprocket
     {
-        private Regex _command = new Regex("^poll (?<room>[a-zA-Z0-9_-]+) (?<question>.+)$");
-
+        private Regex _pollCommand = new Regex("^poll (?<room>[a-zA-Z0-9_-]+) (?<question>.+)$");
+        private Regex _voteCommand = new Regex("^vote (?<vote>[0-9]+)$");
+        private Dictionary<string, Dictionary<int, int>> _polls = new Dictionary<string,Dictionary<int,int>>();
+        private Dictionary<string, Timer> _pollTimers = new Dictionary<string, Timer>();
+        private IBot _bot;
+        
         public bool Handle(ChatMessage message, IBot bot)
         {
-            var pollMatch = _command.Match(message.Content);
-
+            var pollMatch = _pollCommand.Match(message.Content);
             if (pollMatch.Success && message.Receiver != bot.Name) return false;
-            if (!pollMatch.Success) return false;
+            
+            var voteMatch = _voteCommand.Match(message.Content);
+            if (!pollMatch.Success && !voteMatch.Success) return false;
+            if (voteMatch.Success && message.Receiver == bot.Name) return false;
 
-            string broadcast = "A poll has started: " + pollMatch.Groups["question"];
-            bot.Say(broadcast, pollMatch.Groups["room"].ToString());
+            if (pollMatch.Success)
+            {
+                _bot = bot;
+                string room = pollMatch.Groups["room"].Value;
+
+                if (_polls.ContainsKey(room))
+                {
+                    bot.PrivateReply(message.Sender, "A poll is already in effect for " + room +".");
+                }
+                else
+                {
+                    _polls.Add(room, new Dictionary<int, int>());
+#if DEBUG
+                    _pollTimers.Add(room, new Timer(250));
+#else
+                    _pollTimers.Add(room, new Timer(2 * 60 * 1000));
+#endif
+
+                    string broadcast = "A poll has started: " + pollMatch.Groups["question"].Value;
+                    bot.Say(broadcast, room);
+                    bot.Say("Poll will close in 2 minutes.", room);
+
+                    _pollTimers[room].Elapsed += (s, e) => ClosePollAndSendResults(room);
+                    _pollTimers[room].Start();
+                }
+            }
+            else if (voteMatch.Success && _polls.ContainsKey(message.Receiver))
+            {
+                int vote = Convert.ToInt16(voteMatch.Groups["vote"].Value);
+                if (!_polls[message.Receiver].ContainsKey(vote)) _polls[message.Receiver].Add(vote, 0);
+
+                _polls[message.Receiver][vote]++;
+            }
 
             return true;
+        }
+
+        private void ClosePollAndSendResults(string room)
+        {
+            _pollTimers[room].Stop();
+
+            if (!_polls[room].Any())
+            {
+                _bot.Say("No votes were cast for the poll.", room);
+            }
+            else
+            {
+                string message = "poll results ";
+                foreach (var kvp in _polls[room])
+                {
+                    message += string.Format("{0} vote{2} for ({1}). ", kvp.Value, kvp.Key, kvp.Value == 1 ? "" : "s");
+                }
+                _polls.Remove(room);
+                _pollTimers.Remove(room);
+
+                _bot.Say(message.Trim(), room);
+            }
         }
     }
 }
--- Extensions/VotingSprocketTests/VoteSprocketTests.cs
+++ Extensions/VotingSprocketTests/VoteSprocketTests.cs
@@ -58,6 +58,76 @@ namespace VotingSprocketTests
             _sprocket.Handle(message, _bot);
 
             _bot.AssertWasCalled(b => b.Say("A poll has started: Which color to use?", "theroom"));
+            _bot.AssertWasCalled(b => b.Say("Poll will close in 2 minutes.", "theroom"));
+        }
+
+        [TestMethod]
+        public void HandleShouldNotHandleAPrivateVoteMessage()
+        {
+            var message = new ChatMessage("vote 1", "someoneelse", _bot.Name);
+            bool wasHandled = _sprocket.Handle(message, _bot);
+
+            Assert.IsFalse(wasHandled, "Should not have handled the vote.");
+        }
+
+        [TestMethod]
+        public void HandleShouldHandleAPublicVoteMessage()
+        {
+            var message = new ChatMessage("vote 1", "someoneelse", "theroom");
+            bool wasHandled = _sprocket.Handle(message, _bot);
+
+            Assert.IsTrue(wasHandled, "Should have handled the vote.");
+        }
+
+        [TestMethod]
+        public void HandleShouldSayWhenPollIsClosedAndTheVotingResults()
+        {
+            var message = new ChatMessage("poll theroom Which color to use?", "someoneelse", _bot.Name);
+            _sprocket.Handle(message, _bot);
+
+            message.Receiver = "theroom";
+            message.Content = "vote 1";
+            _sprocket.Handle(message, _bot);
+            message.Content = "vote 1"; 
+            _sprocket.Handle(message, _bot);
+            message.Content = "vote 2"; 
+            _sprocket.Handle(message, _bot);
+
+#if DEBUG
+            System.Threading.Thread.Sleep(1000);
+#else
+            System.Threading.Thread.Sleep(2.5 * 60 * 1000);
+#endif
+            _bot.AssertWasCalled(b => b.Say("poll results 2 votes for (1). 1 vote for (2).", "theroom"));
+        }
+
+        [TestMethod]
+        public void SprocketShouldReportNoVotesWhenNoVotesCast()
+        {
+            var message = new ChatMessage("poll theroom Which color to use?", "someoneelse", _bot.Name);
+            _sprocket.Handle(message, _bot);
+            
+
+#if DEBUG
+            System.Threading.Thread.Sleep(1000);
+#else
+            System.Threading.Thread.Sleep(2.5 * 60 * 1000);
+#endif
+            _bot.AssertWasCalled(b => b.Say("No votes were cast for the poll.", "theroom"));
+        }
+
+        // should accept only one vote from a person.
+
+        [TestMethod]
+        public void HandleShouldAllowOnlyOnePollPerRoomAtATime()
+        {
+            var message = new ChatMessage("poll theroom Which color to use?", "someoneelse", _bot.Name);
+            _sprocket.Handle(message, _bot);
+
+            message = new ChatMessage("poll theroom Which food to bake?", "anotherperson", _bot.Name);
+            _sprocket.Handle(message, _bot);
+                        
+            _bot.AssertWasCalled(b => b.PrivateReply("anotherperson", "A poll is already in effect for theroom."));
         }
     }
 }
