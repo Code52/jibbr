c0f036875f7185331ab234b86ea05240f18582de
Stefan D. Poulsen <cyberzed@sleddog.dk>
2012-01-16 19:51:21 +0100

    Added mention posting

--- Extensions/TwitterAnnouncer/TwitterAnnouncer.cs
+++ Extensions/TwitterAnnouncer/TwitterAnnouncer.cs
@@ -19,11 +19,13 @@ namespace TwitterAnnouncer
 
 		private readonly int tweetLimit = 5;
 		private DateTime lastRun = DateTime.Now;
+		private TwitterStatus latestMention;
 		private TwitterStatus latestTweet;
+		private int mentionLimit = 3;
 
 		public TimeSpan Interval
 		{
-			get { return TimeSpan.FromMinutes(10); }
+			get { return TimeSpan.FromMinutes(5); }
 		}
 
 		public void Execute(Bot bot)
@@ -42,27 +44,51 @@ namespace TwitterAnnouncer
 
 			if (latestTweet == null)
 			{
-				latestTweets = twitterService.ListTweetsOnSpecifiedUserTimeline(TwitterUserName, tweetLimit).ToList();
+				latestTweets = twitterService.ListTweetsOnUserTimeline(tweetLimit).ToList();
 			}
 			else
 			{
-				latestTweets = twitterService.ListTweetsOnSpecifiedUserTimelineSince(TwitterUserName, latestTweet.Id, tweetLimit).ToList();
+				latestTweets = twitterService.ListTweetsOnUserTimelineSince(latestTweet.Id, tweetLimit).ToList();
 			}
 
-			if (!latestTweets.Any())
+			if (latestTweets.Any())
 			{
-				return;
-			}
+				latestTweet = latestTweets.First();
 
-			latestTweet = latestTweets.First();
-
-			foreach (var room in bot.Rooms)
-			{
-				bot.Say(string.Format("Latests tweets from @{0}", TwitterUserName), room);
-
-				foreach (var tweet in latestTweets)
+				foreach (var room in bot.Rooms)
 				{
-					bot.Say(tweet.TextDecoded, room);
+					bot.Say(string.Format("Latests tweets from @{0}", TwitterUserName), room);
+
+					foreach (var tweet in latestTweets)
+					{
+						bot.Say(tweet.TextDecoded, room);
+					}
+				}
+			}
+
+			List<TwitterStatus> latestMentions;
+
+			if (latestMention == null)
+			{
+				latestMentions = twitterService.ListTweetsMentioningMe(mentionLimit).ToList();
+			}
+			else
+			{
+				latestMentions = twitterService.ListTweetsMentioningMeSince(latestMention.Id, mentionLimit).ToList();
+			}
+
+			if (latestMentions.Any())
+			{
+				latestMention = latestMentions.First();
+
+				foreach (var room in bot.Rooms)
+				{
+					bot.Say(string.Format("Latest mentions of @{0}", "cyberzeddk"), room);
+
+					foreach (var tweet in latestMentions)
+					{
+						bot.Say(tweet.TextDecoded, room);
+					}
 				}
 			}
 		}
--- Jabbot.ConsoleBotHost/get-packages.cmd
+++ Jabbot.ConsoleBotHost/get-packages.cmd
@@ -3,6 +3,6 @@ set output_dir=%solution_dir%\Jabbot.ConsoleBotHost\bin\Debug\Sprockets\
 
 del %solution_dir%\Jabbot.ConsoleBotHost\bin\Debug\Sprockets\*.dll
 
-for /D %%I in ("%solution_dir%\Extensions\*") do (
+for /D %%I in ( %solution_dir%\Extensions\* ) do (
 xcopy %%I\bin\Debug\*.dll %output_dir% /C /Y
 )
