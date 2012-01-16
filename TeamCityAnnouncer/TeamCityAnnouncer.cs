1b18e03309414c9af52e6dfd5a3bd2918d946c50
Rodrigo Vidal <rodrigovidal777@gmail.com>
2012-01-16 07:08:47 -0300

    TeamCityAnnouncer project created and added TeamCitySharp reference

---
+++ TeamCityAnnouncer/TeamCityAnnouncer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Jabbot.Sprockets.Core;
+using Jabbot;
+
+namespace TeamCityAnnouncer
+{
+    public class TeamCityAnnouncer : IAnnounce
+    {
+
+        public TimeSpan Interval
+        {
+            get { return TimeSpan.FromMinutes(10); }
+        }
+
+        public void Execute(Bot bot)
+        {
+         
+        }
+    }
+}
