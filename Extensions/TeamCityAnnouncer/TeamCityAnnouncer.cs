ec4d860c85313505d830891565bebeef21fbacee
Rodrigo Vidal <rodrigovidal777@gmail.com>
2012-01-17 04:06:00 -0300

    TeamCityAnnouncerRefactoring

--- Extensions/TeamCityAnnouncer/TeamCityAnnouncer.cs
+++ Extensions/TeamCityAnnouncer/TeamCityAnnouncer.cs
@@ -5,63 +5,82 @@ using Jabbot;
 using Jabbot.Sprockets.Core;
 using TeamCitySharp;
 using TeamCitySharp.DomainEntities;
-using TeamCityAnnouncer.Extensions;
 
 namespace TeamCityAnnouncer
 {
     public class TeamCityAnnouncer : IAnnounce
     {
-        public const string ipAddressOrHostNameOfCCServer = "teamcity.codebetter.com";
-
+        public const string IpAddressOrHostNameOfCCServer = "teamcity.codebetter.com";
         public static readonly string[] Projects = { };
-        public static Dictionary<Project, Build> MappedProjects = new Dictionary<Project, Build>();
+        public static List<ProjectModel> MappedProjects = new List<ProjectModel>();
+
+        private TeamCityClient _client;
 
         public TimeSpan Interval
         {
             get { return TimeSpan.FromMinutes(10); }
         }
 
-        private void NotifyRooms(Bot bot, Project project, Build lastBuild)
+        private void NotifyRooms(Bot bot, ProjectModel project)
         {
             foreach (var room in bot.Rooms)
             {
                 bot.Say(
                     string.Format("TeamCity Build Server: {0} / {1} - {2} ({3}) ",
-                                  project.WebUrl,
-                                  project.Name,
-                                  lastBuild.Status,
-                                  lastBuild.FormattedStartDate()), room);
+                                  project.BuildConfigName,
+                                  project.ProjectName,
+                                  project.LastBuildTime,
+                                  project.LastBuildStatus), room);
+            }
+        }
+
+        private void Update(Bot bot, ProjectModel project)
+        {
+            if (Projects.Contains(project.ProjectName))
+            {
+                if (MappedProjects.All(x => x.ProjectName != project.ProjectName))
+                {
+                    MappedProjects.Add(project);
+                    NotifyRooms(bot, project);
+                }
+                else
+                {
+                    var mappedProject = MappedProjects.FirstOrDefault(x => x.ProjectName == project.ProjectName);
+                    if (mappedProject != null)
+                    {
+                        if (project.LastBuildTime != mappedProject.LastBuildTime)
+                        {
+                            MappedProjects.Remove(mappedProject);
+                            MappedProjects.Add(project);
+                            NotifyRooms(bot, project);
+                        }
+                    }
+                }
             }
         }
 
         public void Execute(Bot bot)
         {
-            var client = new TeamCityClient(ipAddressOrHostNameOfCCServer);
-            client.Connect("teamcitysharpuser", "qwerty");
-            var projects = client.AllProjects();
+            if (string.IsNullOrWhiteSpace(IpAddressOrHostNameOfCCServer) || !Projects.Any()) return;
 
-            foreach (var project in projects)
+            _client = new TeamCityClient(IpAddressOrHostNameOfCCServer);
+            _client.Connect("teamcitysharpuser", "qwerty");
+
+            var allProjects = _client.AllProjects();
+            var allBuildConfigs = _client.AllBuildConfigs();
+
+            foreach (var proj in allProjects)
             {
-                if (Projects.Contains(project.Name))
+                Project currentProject = proj;
+                var buildConfigs = allBuildConfigs.Where(buildConfig => currentProject.Id == buildConfig.Project.Id);
+
+                foreach (var currentBuildConfig in buildConfigs)
                 {
-                    var mappedProject = MappedProjects.FirstOrDefault(x => x.Key.Name == project.Name);
-                    if (mappedProject.Key == null)
-                    {
-                        var build = client.GetLastBuild(project);
-                        MappedProjects.Add(project, build);
-                        NotifyRooms(bot, project, build);
-                    }
-                    else
-                    {
-                        var mappedProjectBuild = mappedProject.Value;
-                        var onlineProjectBuild = client.GetLastBuild(project);
-                        if (mappedProjectBuild.StartDate != onlineProjectBuild.StartDate)
-                        {
-                            MappedProjects.Remove(mappedProject.Key);
-                            MappedProjects.Add(project, onlineProjectBuild);
-                            NotifyRooms(bot, project, onlineProjectBuild);
-                        }
-                    }
+                    var build = _client.LastBuildByBuildConfigId(currentBuildConfig.Id);
+
+                    var project = new ProjectModel(currentProject.Name, currentProject.Id, currentBuildConfig.Name, build.StartDate,
+                                                     build.Status, build.StatusText);
+                    Update(bot, project);
                 }
             }
         }
